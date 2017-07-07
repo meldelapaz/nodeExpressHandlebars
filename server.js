0a4bcbb19c169c7ba2b9545d6a1e69d471f98d3a
meldelapaz <meldelapaz.17@gmail.com>
2017-07-07 09:52:32 -0400

    added heroku code

--- server.js
+++ server.js
@@ -30,6 +30,23 @@ var connection = mysql.createConnection({
     database: "SNL"
 });
 
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+
+connection.connect();
+module.exports = connection;
+
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
